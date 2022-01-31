7c56fc8eb3f596586049cc1c3bebf421929ffcb2
saman-zdf <samanfyz68@gmail.com>
2022-01-31 11:53:13 +0800

    added proxy to the package json for server url to be able fetch data from server without getting CORS error

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,4 +1,20 @@
+import React, { useEffect } from 'react';
+
 const Dashboard = () => {
+  const fetchData = async function () {
+    try {
+      // const res = await fetch('/data.json');
+      const res = await fetch('/api/v1');
+      const data = await res.json();
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
   return <div>Dashboard Page</div>;
 };
 
