4490de9afcb6fb1cb8dc4d0239b48dcee8dc71a8
saman-zdf <samanfyz68@gmail.com>
2022-02-01 08:42:08 +0800

    added sharedLayout of Navbarm and Sidebars for diff screen sizes

---
+++ src/components/BigSidebar.js
@@ -0,0 +1,6 @@
+import Wrapper from '../assets/wrappers/BigSidebar';
+const BigSidebar = () => {
+  return <Wrapper>Big Sidebar</Wrapper>;
+};
+
+export default BigSidebar;
---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import Wrapper from '../assets/wrappers/Navbar';
+import { FaHome } from 'react-icons/fa';
+
+const Navbar = () => {
+  return (
+    <Wrapper>
+      <h1>Navbar</h1>
+      <FaHome />
+    </Wrapper>
+  );
+};
+
+export default Navbar;
