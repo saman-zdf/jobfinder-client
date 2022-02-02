9043bed44813d9c02ddaa3f2451d6c4262c25dcf
saman-zdf <samanfyz68@gmail.com>
2022-02-02 10:44:50 +0800

    navbar layouor has been added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,49 @@
+import React, { useState } from 'react';
 import Wrapper from '../assets/wrappers/Navbar';
-import { FaHome } from 'react-icons/fa';
-
+import {
+  FaAlignLeft,
+  FaUserCircle,
+  FaCareDown,
+  FaCaretDown,
+} from 'react-icons/fa';
+import { useAppContext } from '../context/appContext';
+import Logo from './Logo';
 const Navbar = () => {
   return (
     <Wrapper>
-      <h1>Navbar</h1>
-      <FaHome />
+      <div className='nav-center'>
+        <button
+          type='button'
+          className='toggle-btn'
+          onClick={() => console.log('toggle sidebar!')}
+        >
+          <FaAlignLeft />
+        </button>
+        <div>
+          <Logo />
+          <h3 className='logo-text'>dashboard</h3>
+        </div>
+        <div className='btn-container'>
+          <button
+            className='btn'
+            type='button'
+            onClick={() => console.log('show/hide dropdown')}
+          >
+            <FaUserCircle />
+            Sam
+            <FaCaretDown />
+          </button>
+          <div className='dropdown show-dropdown'>
+            <button
+              type='button'
+              className='dropdown-btn'
+              onClick={() => console.log('logout')}
+            >
+              logout
+            </button>
+          </div>
+        </div>
+      </div>
     </Wrapper>
   );
 };
