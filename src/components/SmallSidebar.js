ffdf14a83275aa047e1837d83ef26fb046e0368d
saman-zdf <samanfyz68@gmail.com>
2022-02-05 07:27:49 +0800

    added links file, added toggle func for small sidebar

--- src/components/SmallSidebar.js
+++ src/components/SmallSidebar.js
@@ -1,7 +1,30 @@
 import Wrapper from '../assets/wrappers/SmallSidebar';
-
+import { FaTimes } from 'react-icons/fa';
+import { useAppContext } from '../context/appContext';
+import links from '../utils/links';
+import { NavLink } from 'react-router-dom';
+import Logo from './Logo';
 const SmallSidebar = () => {
-  return <Wrapper>small sidebar</Wrapper>;
+  const { showSidebar, toggleSidebar } = useAppContext();
+  return (
+    <Wrapper>
+      <div
+        className={
+          showSidebar ? 'sidebar-container show-sidebar' : 'sidebar-container'
+        }
+      >
+        <div className='content'>
+          <button type='button' className='close-btn' onClick={toggleSidebar}>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className='nav-links'>Nav links</div>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default SmallSidebar;
