10b6f0555937ba6b2510f4f8e81439e327e8b604
saman-zdf <samanfyz68@gmail.com>
2022-02-05 17:57:51 +0800

    update request has been added

--- src/context/appContext.js
+++ src/context/appContext.js
@@ -8,6 +8,9 @@ import {
   SETUP_USER_ERROR,
   TOGGLE_SIDEBAR,
   LOGOUT_USER,
+  UPDATE_USER_BEGIN,
+  UPDATE_USER_SUCCESS,
+  UPDATE_USER_ERROR,
 } from './action';
 import { reducer } from './reducer';
 
@@ -111,12 +114,25 @@ const AppProvider = ({ children }) => {
   };
   // update user
   const updateUser = async (currentUser) => {
+    dispatch({ type: UPDATE_USER_BEGIN });
     try {
       const { data } = await authFetch.patch('/auth/updateUser', currentUser);
       console.log(data);
+      const { user, location, token } = data;
+      dispatch({
+        type: UPDATE_USER_SUCCESS,
+        payload: { user, location, token },
+      });
+      addUserToLocalStorage({ user, token, location });
     } catch (error) {
-      console.log(error.response);
+      const {
+        response: {
+          data: { msg },
+        },
+      } = error;
+      dispatch({ type: UPDATE_USER_ERROR, payload: { msg: msg } });
     }
+    clearAlert();
   };
   // sidebar
   const toggleSidebar = () => {
