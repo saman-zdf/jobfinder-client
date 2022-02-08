04bcfc4dc03189f106d790039ea768ec8547f9e5
saman-zdf <samanfyz68@gmail.com>
2022-02-09 07:38:37 +0800

    serach form has been added

--- src/components/SearchContainer.js
+++ src/components/SearchContainer.js
@@ -23,18 +23,54 @@ const SearchContainer = () => {
     let { name, value } = e.target;
     handleChange({ name, value });
   };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    clearFilters();
+  };
   return (
     <Wrapper>
       <form className='form'>
         <h4>Search form</h4>
         <div className='form-center'>
+          {/* search position */}
           <FormRow
             type='text'
             name='search'
             value={search}
             handleChange={handleSearch}
           />
-          {/* rest of inputs */}
+          {/* search by status */}
+          <FormRowSelect
+            labelText='status'
+            name='searchStatus'
+            value={searchStatus}
+            handleChange={handleSearch}
+            list={['all', ...statusOptions]}
+          />
+          {/* sort by type */}
+          <FormRowSelect
+            labelText='Job type'
+            name='searchType'
+            value={searchType}
+            handleChange={handleSearch}
+            list={['all', ...jobTypeOptions]}
+          />
+          {/* sort */}
+          <FormRowSelect
+            name='sort'
+            value={sort}
+            handleChange={handleSearch}
+            list={sortOptions}
+          />
+          <button
+            className='btn btn-block btn-danger'
+            disabled={isLoading}
+            onClick={handleSubmit}
+            type='submit'
+          >
+            Clear filters
+          </button>
         </div>
       </form>
     </Wrapper>
